96f050c2e03cddde6a4f9c098bc18f529195761b
Nicolas Fernandez Ruoff <134111737+NicoferUtn@users.noreply.github.com>
2023-05-24 17:30:10 -0300

    Update main.cpp

--- 01-CalcularPI/main.cpp
+++ 01-CalcularPI/main.cpp
@@ -1,6 +1,3 @@
-//Se desea calcular el valor de π usando la serie de Liebniz, sin usar ninguna optimización.
-//Debe iterar hasta que el valor de π se correcto en sus 6 primeros decimales (3,141592)
-
 #include<iostream>
 #include<iomanip>
 using namespace std;
